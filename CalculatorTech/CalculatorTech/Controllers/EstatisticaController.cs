295fd2967e362eaf8dd6693bfbc646dabab934f3
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2023-11-02 12:01:52 -0300

    Inclusão do grid de distribuição de frequência

---
+++ CalculatorTech/CalculatorTech/Controllers/EstatisticaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorTech.Controllers
+{
+    public class EstatisticaController : Controller
+    {
+        public IActionResult DistribuicaoFrequencia()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Calcular(String[] valor)
+        {
+
+            return View();
+        }
+    }
+}
